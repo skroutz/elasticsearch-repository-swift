216780761ac5f5293392a80349e94d6a2c3f9891
Alexander Leyke <alexander_leyke@ultimatesoftware.com>
2019-02-27 15:46:53 -0500

    Alex - create blob store lazily

--- src/main/java/org/wikimedia/elasticsearch/swift/repositories/SwiftRepository.java
+++ src/main/java/org/wikimedia/elasticsearch/swift/repositories/SwiftRepository.java
@@ -53,9 +53,6 @@ public class SwiftRepository extends BlobStoreRepository {
         Setting<Boolean> COMPRESS_SETTING = Setting.boolSetting("compress", false);
     }
 
-    // Our blob store instance
-    private final SwiftBlobStore blobStore;
-
     // Base path for blobs
     private final BlobPath basePath;
 
@@ -64,6 +61,9 @@ public class SwiftRepository extends BlobStoreRepository {
 
     // Are we compressing our snapshots?
     private final boolean compress;
+
+    private final Settings settings;
+    private final SwiftService swiftService;
 
     /**
      * Constructs new BlobStoreRepository
@@ -81,42 +81,35 @@ public class SwiftRepository extends BlobStoreRepository {
     public SwiftRepository(RepositoryMetaData metadata, Settings settings,
                            NamedXContentRegistry namedXContentRegistry, SwiftService swiftService) {
         super(metadata, settings, namedXContentRegistry);
+        this.settings = settings;
+        this.swiftService = swiftService;
+        this.chunkSize = Swift.CHUNK_SIZE_SETTING.get(metadata.settings());
+        this.compress = Swift.COMPRESS_SETTING.get(metadata.settings());
+        this.basePath = BlobPath.cleanPath();
+    }
 
-        String url = Swift.URL_SETTING.get(metadata.settings());
-        if (url == null) {
-            throw new RepositoryException(metadata.name(), "No url defined for swift repository");
-        }
+    @Override
+    protected BlobStore createBlobStore() throws Exception {
+        String username = Swift.USERNAME_SETTING.get(metadata.settings());
+        String password = Swift.PASSWORD_SETTING.get(metadata.settings());
+        String tenantName = Swift.TENANTNAME_SETTING.get(metadata.settings());
+        String authMethod = Swift.AUTHMETHOD_SETTING.get(metadata.settings());
+        String preferredRegion = Swift.PREFERRED_REGION_SETTING.get(metadata.settings());
 
         String container = Swift.CONTAINER_SETTING.get(metadata.settings());
         if (container == null) {
             throw new RepositoryException(metadata.name(), "No container defined for swift repository");
         }
 
-        String username = Swift.USERNAME_SETTING.get(metadata.settings());
-        String password = Swift.PASSWORD_SETTING.get(metadata.settings());
-        String tenantName = Swift.TENANTNAME_SETTING.get(metadata.settings());
-        String authMethod = Swift.AUTHMETHOD_SETTING.get(metadata.settings());
-        String preferredRegion = Swift.PREFERRED_REGION_SETTING.get(metadata.settings());
+        String url = Swift.URL_SETTING.get(metadata.settings());
+        if (url == null) {
+            throw new RepositoryException(metadata.name(), "No url defined for swift repository");
+        }
+
         Account account = SwiftAccountFactory.createAccount(swiftService, url, username, password, tenantName,
                 authMethod, preferredRegion);
 
-        blobStore = new SwiftBlobStore(settings, account, container);
-        this.chunkSize = Swift.CHUNK_SIZE_SETTING.get(metadata.settings());
-        this.compress = Swift.COMPRESS_SETTING.get(metadata.settings());
-        this.basePath = BlobPath.cleanPath();
-    }
-
-    /**
-     * Get the blob store
-     */
-    @Override
-    protected BlobStore blobStore() {
-        return blobStore;
-    }
-
-    @Override
-    protected BlobStore createBlobStore() throws Exception {
-        throw new UnsupportedOperationException(); //TODO implement
+        return new SwiftBlobStore(settings, account, container);
     }
 
     /**
